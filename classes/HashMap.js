b423f0353e1e50adb25112c69b835615f212b07d
Fahim Ahmed <fahim7711@gmail.com>
2024-05-27 23:16:07 +0100

    Refactored HashMap into class factory and added set function

--- classes/HashMap.js
+++ classes/HashMap.js
@@ -1,11 +1,31 @@
-function hash(key) {
-    let hashCode = 0;
-
-    const primeNumber = 31;
-
-    for (let i = 0; i < key.length; i++) {
-        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+export class HashMap {
+    constructor(size = 40) {
+        this.keyMap = new Array(size);
+        this.size = 0;
     }
 
-    return hashCode;
+    hash(key) {
+        let hashCode = 0;
+    
+        const primeNumber = 31;
+    
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            hashCode = (primeNumber * hashCode + value) % this.keyMap.length;
+        }
+    
+        console.log(hashCode);
+        return hashCode;
+    }
+    
+    set(key, value) {
+        let index = this.hash(key);
+
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+        console.log(this.keyMap[index]);
+    }
 }
---
+++ script.js
@@ -0,0 +1,5 @@
+import { HashMap } from "./classes/HashMap.js";
+
+let hashmap = new HashMap();
+
+hashmap.set("key1", "marco");
